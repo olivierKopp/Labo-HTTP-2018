7ab6f8e08f9b47a2d9429f2fa1002d759453bc55
olivier_kopp <olivier.kopp@heig-vd.ch>
2018-06-14 23:42:10 +0200

    adding load balancing and sticky session

---
+++ docker-images/load-balancing-image/express-image2/src/index.js
@@ -0,0 +1,25 @@
+var Chance = require('chance');
+var chance = new Chance();
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send(generateAnimals());
+});
+
+app.listen(3000, function(){
+	console.log('Accepting HTTP request on port 3000')
+});
+
+function generateAnimals(){
+	var numberOfAnimals = 15;
+	var animals = [];
+	
+	for(var i = 0; i < numberOfAnimals; i++){
+		animals.push(chance.animal() + "2");
+	}
+	
+	console.log(animals);
+	return animals;
+}
